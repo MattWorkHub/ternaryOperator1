e56f64d504746a7dd0ee7c52294357da43734fd2
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2024-01-12 14:03:04 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        String result = value.length() >= 10 ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+        return result;
+    }
+
+}
